2ed455fd74c73051506a502219f34182b64db85d
GitHub Action <action@github.com>
2021-03-02 16:02:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/zeratoxx/gitgrep/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/zeratoxx/gitgrep/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zeratoxx.gitgrep.listeners
 
+import com.github.zeratoxx.gitgrep.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/zeratoxx/gitgrep/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zeratoxx/gitgrep/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zeratoxx.gitgrep.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zeratoxx.gitgrep.MyBundle
 
 class MyApplicationService {
 
